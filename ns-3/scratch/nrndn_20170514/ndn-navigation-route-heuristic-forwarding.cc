042e25dc57f9f0283ee8170bc2387a649a7a8538
Sindyang <863326066@qq.com>
2017-06-22 15:51:10 +0800

    send interest packet

--- ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
+++ ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
@@ -833,11 +833,11 @@ void NavigationRouteHeuristic::SendInterestPacket(Ptr<Interest> interest)
 	if(!m_running) return;
 
 	//added by sy
-    ndn::nrndn::nrHeader nrheader;
+    /*ndn::nrndn::nrHeader nrheader;
     interest->GetPayload()->PeekHeader(nrheader);
     uint32_t nodeId = nrheader.getSourceId();
 	uint32_t myNodeId = m_node->GetId();
-	cout<<"(forwarding.cc-SendInterestPacket) 兴趣包的源节点为 "<<nodeId<<",转发该兴趣包的节点为 "<<myNodeId<<endl;
+	cout<<"(forwarding.cc-SendInterestPacket) 兴趣包的源节点为 "<<nodeId<<",转发该兴趣包的节点为 "<<myNodeId<<endl;*/
 
 	if(HELLO_MESSAGE!=interest->GetScope()||m_HelloLogEnable)
 		NS_LOG_FUNCTION (this);
