225d0313dd60de4e74f21e8e9324d45e03caf8dc
Sindyang <863326066@qq.com>
2017-07-06 15:57:47 +0800

    add rsu pit

--- ns-3/scratch/nrndn_20170514/ndn-nr-pit-impl.cc
+++ ns-3/scratch/nrndn_20170514/ndn-nr-pit-impl.cc
@@ -216,16 +216,15 @@ void NrPitImpl::DeleteFrontNode(uint32_t& id)
 	{
 		Ptr<EntryNrImpl> pitEntry = DynamicCast<EntryNrImpl>(*pit);
 		pitEntry->CleanPITNeighbors(id);
+		//要是有很多空的PIT 可以加一个删除PIT的操作
+		if(interestNodes.empty())
+		{
+			const name::Component &pitName=pitEntry->GetInterest()->GetName().get(0);
+			std::string pitname = pitName.toUri();
+			std::cout<<"(ndn-nr-pit-impl.cc-DeleteFrontNode) PIT中 "<<pitname<<" 为空"<<std::endl;
+		}
 	}
-	//要是有很多空的PIT 可以加一个删除PIT的操作
-	const std::unordered_set<uint32_t>& interestNodes = entry->getIncomingnbs();
-	if(interestNodes.empty())
-	{
-		const name::Component &pitName=entry->GetInterest()->GetName().get(0);
-		std::string pitname = pitName.toUri();
-		cout<<"(ndn-nr-pit-impl.cc-DeleteFrontNode) PIT中 "<<pitname<<" 为空"<<endl;
-		getchar();
-	}
+	getchar();
 }
 
 void 
