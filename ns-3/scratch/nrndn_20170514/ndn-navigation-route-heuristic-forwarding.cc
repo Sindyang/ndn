e626e85670c4f9a706836d0d938a8a3bcaa89272
Sindyang <863326066@qq.com>
2017-07-11 12:53:34 +0800

    change processhello

--- ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
+++ ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
@@ -1092,10 +1092,10 @@ void NavigationRouteHeuristic::ProcessHello(Ptr<Interest> interest)
 			else if(!msg.first)
 			{
 				cout<<"(forwarding.cc-ProcessHello) 转发节点位于其他路段"<<endl;
-				//if(m_nbChange_mode > 1)
-				//{
+				if(m_nbChange_mode > 1)
+				{
 					notifyUpperOnInterest();
-				//}
+				}
 			}
 		}
 	}
