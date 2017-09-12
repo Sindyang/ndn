bd1ac750de8a3d65e9f796429e88b8659618dd07
Sindyang <863326066@qq.com>
2017-09-12 20:23:30 +0800

    change prioritylist

--- ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
+++ ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
@@ -1198,7 +1198,7 @@ void NavigationRouteHeuristic::ProcessHello(Ptr<Interest> interest)
 			cout<<"转发节点全部丢失"<<endl;
 			notifyUpperOnInterest();
 		}
-		else if(msgdirection.first && msgdirection.second && m_nbChange_mode > 1)
+		else if(msgdirection.first && msgdirection.second)
 		{
 			cout<<"转发节点部分丢失，但有新的邻居进入"<<endl;
 			notifyUpperOnInterest();
