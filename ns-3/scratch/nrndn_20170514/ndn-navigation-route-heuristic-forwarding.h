a91a0e2cf9c58f7ea912e0951f3d6cf6b2059716
Sindyang <863326066@qq.com>
2018-01-04 18:00:24 +0800

    m_cs

--- ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.h
+++ ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.h
@@ -518,10 +518,6 @@ private:
 
 	Ptr<pit::nrndn::NrPitImpl> m_nrpit; ///< \brief Reference to PIT to which this forwarding strategy is associated
 	
-	Ptr<ndn::cs::nrndn::NrCsImpl> m_csdata;
-	
-	Ptr<ndn::cs::nrndn::NrCsInterestImpl> m_csinterest;
-	
 	Ptr<ndn::cs::nrndn::NrCsImpl> m_cs;
 
 	uint32_t				m_CacheSize;
