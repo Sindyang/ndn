ec40fd9c780b3aa7999e36a5a1c759eed0b6da4e
Sindyang <863326066@qq.com>
2018-01-09 15:30:27 +0800

    get data in cache

--- ns-3/scratch/nrndn_20170514/ndn-nr-cs-impl.cc
+++ ns-3/scratch/nrndn_20170514/ndn-nr-cs-impl.cc
@@ -191,13 +191,12 @@ NrCsImpl::GetData(std::unordered_map<std::string,std::unordered_set<std::string>
 		//从缓存中取出对应的数据包
 		for(;itdataroute != dataname_route.end();itdataroute++)
 		{
-			std::cout<<"(cs-impl.cc-GetData) 想要获得的数据包名称为 "<<itdataroute->first<<std::endl;
 			for(it = m_data.begin();it != m_data.end();it++)
 			{
 				std::string dataname = it->second->GetName().toUri();
-				std::cout<<"(cs-impl.cc-GetData) 从缓存中得到的数据包为 "<<dataname<<std::endl;
 				if(itdataroute->first == dataname)
 				{
+					std::cout<<"(cs-impl.cc-GetData) 从缓存中得到的数据包为 "<<dataname<<std::endl;
 					Ptr<const Data> data = it->second->GetData();
 					DataCollection[data->GetSignature()] = data;
 				}
