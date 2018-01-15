6e8f5d3cc4a9c2e4ccf9d2ccd9295dab8ff80502
Sindyang <863326066@qq.com>
2018-01-15 20:32:07 +0800

    delete send data in cache

--- ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
+++ ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
@@ -689,7 +689,7 @@ void NavigationRouteHeuristic::OnInterest_RSU(Ptr<Face> face,Ptr<Interest> inter
 		
 		if(!datacollection.empty())
 		{
-			//SendDataInCache(datacollection);
+			SendDataInCache(datacollection);
 			cout<<"(forwarding.cc-OnInterest_RSU) 从缓存中取出数据包"<<endl;
 		}
 		
@@ -1964,7 +1964,7 @@ void NavigationRouteHeuristic::ProcessHello(Ptr<Interest> interest)
 			map<uint32_t,Ptr<const Data> > datacollection = m_cs->GetData();
 			if(!datacollection.empty())
 			{
-				//SendDataInCache(datacollection);
+				SendDataInCache(datacollection);
 				cout<<"(forwarding.cc-ProcessHello) 从缓存中取出数据包"<<endl;
 			}
 			
