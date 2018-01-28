084e938f0ceba8c517d7a1e271e94ef71e6a3182
Sindyang <863326066@qq.com>
2018-01-28 15:09:45 +0800

    main-setcontentstore

--- ns-3/scratch/nrndn_20170514/main.cc
+++ ns-3/scratch/nrndn_20170514/main.cc
@@ -706,7 +706,7 @@ nrndnExample::InstallNrNdnStack()
 	cout<<"(main.cc-InstallNrNdnStack)pitInterval = "<<pitCleanIntervalStr.str()<<endl;
 	ndnHelper.SetForwardingStrategy ("ns3::ndn::fw::nrndn::NavigationRouteHeuristic","HelloLogEnable",str,"NoFwStop",noFwStopStr,"TTLMax",TTLMaxStr.str());
 	cout<<"(main.cc-InstallNrNdnStack) SetForwardingStrategy"<<endl;
-	ndnHelper.SetContentStore("ns3::ndn::cs::Lru", "MaxSize", "1000");
+	//ndnHelper.SetContentStore("ns3::ndn::cs::Lru", "MaxSize", "1000");
 	ndnHelper.SetContentStore("ns3::ndn::cs::nrndn::NrCsImpl");
 	cout<<"(main.cc-InstallNrNdnStack) SetContentStore"<<endl;
 	ndnHelper.SetPit("ns3::ndn::pit::nrndn::NrPitImpl","CleanInterval",pitCleanIntervalStr.str());
