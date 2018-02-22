d916b440cd84787e338397625e188b1a5de79ca1
Sindyang <863326066@qq.com>
2018-02-22 12:14:19 +0800

    add get shortest path

--- ns-3/scratch/nrndn_20170514/getShortestPath.h
+++ ns-3/scratch/nrndn_20170514/getShortestPath.h
@@ -55,6 +55,7 @@ public:
 	Ptr<vanetmobility::sumomobility::SumoMobility> m_sumodata;
 	map<string,int> m; 			//顶点与下标的对应关系
 	vector<Edges> G[nV];          // 图的邻接表形式
+	int G1[nV][nV];               // 图的邻接矩阵形式
 	int dist[nV];                // 从源点出发的最短距离
 	typedef pair<int, int> P;   // first是最短距离，second是顶点编号
 	bool vis[nV];
