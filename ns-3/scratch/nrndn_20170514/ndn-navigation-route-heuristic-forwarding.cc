231c58283bc655e4b150fa14ea9a8f4730fa9763
Sindyang <863326066@qq.com>
2018-03-14 14:58:16 +0800

    delete setnonce

--- ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
+++ ns-3/scratch/nrndn_20170514/ndn-navigation-route-heuristic-forwarding.cc
@@ -986,7 +986,6 @@ NavigationRouteHeuristic::Interest_InInterestRoute(Ptr<Interest> interest,vector
 			}
 			cout<<"当前节点 "<<myNodeId<<" 源节点 "<<nodeId<<" 重新选择后的实际转发路线为 "<<newforwardRoute<<endl;
 			//重新设置序列号
-			interest->SetNonce(m_rand.GetValue());
 			interest->SetRoutes(newforwardRoute);
 			m_sendingInterestEvent[nodeId][seq] = Simulator::Schedule(sendInterval,&NavigationRouteHeuristic::ForwardInterestPacket,this,interest,anotherNewPriorityList);
 		}
