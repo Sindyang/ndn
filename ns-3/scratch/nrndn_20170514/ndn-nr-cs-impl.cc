80e34b777bce8baebfdb726d152b035aa1f4bf0f
Sindyang <863326066@qq.com>
2018-03-15 16:52:33 +0800

    get interest

--- ns-3/scratch/nrndn_20170514/ndn-nr-cs-impl.cc
+++ ns-3/scratch/nrndn_20170514/ndn-nr-cs-impl.cc
@@ -567,7 +567,7 @@ NrCsImpl::GetInterest(std::string lane)
 	{
 		Ptr<const Interest> interest = it->second->GetInterest();
 		std::string routes = interest->GetRoutes(); 
-		//std::cout<<"(cs-interest.cc-GetInterest) 兴趣包实际导航路线 "<<routes<<std::endl;
+		std::cout<<"(cs-interest.cc-GetInterest) 兴趣包的nonce为 "<<interest->GetNonce()<<" 实际导航路线 "<<routes;
 		std::string currentroute = routes.substr(0,lane.length());
 		//std::cout<<"(cs-impl.cc-GetInterest) 兴趣包下一行驶路段 "<<currentroute<<std::endl;
 		//getchar();
