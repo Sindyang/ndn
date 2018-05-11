86a017b9d6be455dd2c97547feb7610ed80be77e
Sindyang <863326066@qq.com>
2018-05-11 20:46:49 +0800

    change distance

--- ns-3/scratch/nrndn_siukwan/main.cc
+++ ns-3/scratch/nrndn_siukwan/main.cc
@@ -202,8 +202,8 @@ int main (int argc, char **argv)
 //构造函数
 nrndnExample::nrndnExample () :
   random_seed(54321),
-  certain_count(15), //定点数量
-  certain_interval(10),//定点事件间隔
+  certain_count(33), //定点数量
+  certain_interval(4),//定点事件间隔
   random_accident(0),//默认不随机
   size (3),
   totalTime (36000),
