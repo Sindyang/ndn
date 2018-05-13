c8e1e86048793f4ffd66562bba6215e75328dfd4
Sindyang <863326066@qq.com>
2018-05-13 14:49:34 +0800

    change siukwan's

--- ns-3/scratch/nrndn_siukwan/main.cc
+++ ns-3/scratch/nrndn_siukwan/main.cc
@@ -940,12 +940,12 @@ void nrndnExample::InstallTraffics()
 	}
 	else
 	{
-		//uint32_t array[15] = {1,79,93,163,270,343,365,377,396,413,497,606,666,783,784};
+		uint32_t array[15] = {11,13,18,25,34,49,63,66,127,149,156,163,165,179,207};
 		for(uint32_t index = 0; index < certain_count; index++)
 		{
 
-			//randomNode[array[index]] = true;
-			randomNode[index] = true;
+			randomNode[array[index]] = true;
+			//randomNode[index] = true;
 			Ptr<ns3::ndn::nrndn::nrProducer> producer= DynamicCast<ns3::ndn::nrndn::nrProducer>(
 					nodes.Get(array[index])->GetApplication(nrUtils::appIndex["ns3::ndn::nrndn::nrProducer"]));
 			NS_ASSERT(producer);
