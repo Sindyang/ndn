19b70a08d3c80252449ee14f6cc2a6d1cdc12f31
Sindyang <863326066@qq.com>
2018-05-17 11:20:13 +0800

    change certain_count

--- ns-3/scratch/nrndn_siukwan/main.cc
+++ ns-3/scratch/nrndn_siukwan/main.cc
@@ -202,8 +202,8 @@ int main (int argc, char **argv)
 //构造函数
 nrndnExample::nrndnExample () :
   random_seed(54321),
-  certain_count(54), //定点数量
-  certain_interval(5),//定点事件间隔
+  certain_count(55), //定点数量
+  certain_interval(4),//定点事件间隔
   random_accident(0),//默认不随机
   size (3),
   totalTime (36000),
