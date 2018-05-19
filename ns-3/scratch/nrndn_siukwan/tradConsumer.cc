a19a8729fc814eda714833abbc7a265666bbb6fc
Sindyang <863326066@qq.com>
2018-05-19 15:31:42 +0800

    change siukwan

--- ns-3/scratch/nrndn_siukwan/tradConsumer.cc
+++ ns-3/scratch/nrndn_siukwan/tradConsumer.cc
@@ -98,6 +98,9 @@ void tradConsumer::OnData(Ptr<const Data> data)
 	
 	if(IsInterestData(data->GetName()))
 	{
+		//2018.5.19 
+		if(m_node->GetId() >= 600)
+			return;
 		nrUtils::IncreaseInterestedNodeCounter(nodeId,signature);
 		// 2018.1.25 只统计感兴趣的延迟
 		nrUtils::InsertTransmissionDelayItem(nodeId,signature,delay);
