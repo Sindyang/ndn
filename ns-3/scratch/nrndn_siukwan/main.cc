1f49f28fbb878dac146d7663e9334f73679fd7ba
Sindyang <863326066@qq.com>
2018-05-19 08:38:16 +0800

    change start time

--- ns-3/scratch/nrndn_siukwan/main.cc
+++ ns-3/scratch/nrndn_siukwan/main.cc
@@ -910,7 +910,7 @@ void nrndnExample::InstallTestApplications()
 void nrndnExample::InstallTraffics()
 {
 	SeedManager::SetSeed(random_seed);
-	UniformVariable rnd(0,nodes.GetN()-527);
+	UniformVariable rnd(0,nodes.GetN()-298);
 	std::cout<<"插入事件："<<accidentNum<<endl;
 	if(random_accident)
 	{
