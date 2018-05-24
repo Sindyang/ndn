b935e46bf055adb6c4c60754261d4ff77acef53c
Sindyang <863326066@qq.com>
2018-05-24 15:15:34 +0800

    change producer number to 55

--- ns-3/scratch/nrndn_siukwan/main.cc
+++ ns-3/scratch/nrndn_siukwan/main.cc
@@ -202,7 +202,7 @@ int main (int argc, char **argv)
 //构造函数
 nrndnExample::nrndnExample () :
   random_seed(54321),
-  certain_count(50), //定点数量
+  certain_count(55), //定点数量
   certain_interval(30),//定点事件间隔
   random_accident(0),//默认不随机
   size (3),
