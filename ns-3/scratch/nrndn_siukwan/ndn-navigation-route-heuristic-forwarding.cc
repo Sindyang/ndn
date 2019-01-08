7db59d61be950ea67c8bbde6e0b39a663ef2fba2
Sindyang <863326066@qq.com>
2019-01-08 20:29:27 +0800

    change timevalue

--- ns-3/scratch/nrndn_siukwan/ndn-navigation-route-heuristic-forwarding.cc
+++ ns-3/scratch/nrndn_siukwan/ndn-navigation-route-heuristic-forwarding.cc
@@ -42,7 +42,7 @@ TypeId NavigationRouteHeuristic::GetTypeId(void)
 							.SetParent<GreenYellowRed>()
 							.AddConstructor<NavigationRouteHeuristic>()
 							.AddAttribute("HelloInterval", "HELLO messages emission interval.",
-										  TimeValue(Seconds(5)),
+										  TimeValue(Seconds(1)),
 										  MakeTimeAccessor(&NavigationRouteHeuristic::HelloInterval),
 										  MakeTimeChecker())
 							.AddAttribute("AllowedHelloLoss", "Number of hello messages which may be loss for valid link.",
