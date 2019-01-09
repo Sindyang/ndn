691fc2dc867765c95857b5a4655af67616f798a9
Sindyang <863326066@qq.com>
2019-01-09 20:13:53 +0800

    change fixed

--- ns-3/scratch/nrndn_new/nrProducer.cc
+++ ns-3/scratch/nrndn_new/nrProducer.cc
@@ -313,7 +313,7 @@ void nrProducer::addAccident(double iType)
 	double start = m_startTime.GetSeconds();
 	double end = m_stopTime.GetSeconds();
 
-	for (double dTime = 200; dTime < 450; dTime += iType)
+	for (double dTime = 100; dTime <= 450; dTime += iType)
 	{
 		ScheduleAccident(dTime);
 	}
--- ns-3/scratch/nrndn_siukwan/nrProducer.cc
+++ ns-3/scratch/nrndn_siukwan/nrProducer.cc
@@ -355,7 +355,7 @@ void nrProducer::addAccident(double iType)
 
 	std::cout << "start " << start << std::endl;
 	std::cout << "end " << end << std::endl;
-	for (double dTime = 100; dTime <= 350; dTime += iType)
+	for (double dTime = 100; dTime <= 450; dTime += iType)
 	{
 		ScheduleAccident(dTime);
 		//std::cout<<"siu:"<<m_node->GetId()<<" add accident at "<< dTime <<endl;
