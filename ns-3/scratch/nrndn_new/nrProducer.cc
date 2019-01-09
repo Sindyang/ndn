61733fd1efa7d4c8ae6625f57b420c8f13d55db9
Sindyang <863326066@qq.com>
2019-01-09 17:34:41 +0800

    change start and end time

--- ns-3/scratch/nrndn_new/nrProducer.cc
+++ ns-3/scratch/nrndn_new/nrProducer.cc
@@ -284,11 +284,11 @@ void nrProducer::addAccident()
 	double start = m_startTime.GetSeconds();
 	double end = m_stopTime.GetSeconds();
 	SeedManager::SetSeed(54321);
-	UniformVariable nrnd(start, end);
+	UniformVariable nrnd(start + 40, end - 20);
 	//2019.1.6 修改发送数据包的时刻
 	uint32_t t = 0;
 	//2019.1.5 设定每个生产者发送的数据包个数
-	uint32_t count = (end - start) / 20;
+	uint32_t count = (end - 20 - start - 40) / 20;
 	std::cout << "(nrProducer.cc-addAccident) 生产者 " << m_node->GetId() << "预计发送 " << count << "个数据包" << std::endl;
 	while (count--)
 	{
