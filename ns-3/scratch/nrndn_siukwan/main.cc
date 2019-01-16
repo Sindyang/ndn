6362ac5e6e0dbcb94780659c4694fb16b32f0bfe
Sindyang <863326066@qq.com>
2019-01-16 21:04:20 +0800

    new input

--- ns-3/scratch/nrndn_siukwan/main.cc
+++ ns-3/scratch/nrndn_siukwan/main.cc
@@ -198,7 +198,7 @@ int main(int argc, char **argv)
 //-----------------------------------------------------------------------------
 //构造函数
 nrndnExample::nrndnExample() : random_seed(54321),
-							   certain_count(20),   //定点数量
+							   certain_count(40),   //定点数量
 							   certain_interval(0), //定点事件间隔
 							   random_accident(0),  //默认不随机
 							   size(3),
