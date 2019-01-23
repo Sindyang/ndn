ace6f40ba152f0ddc7796e83c90a92f3aed7dfed
Sindyang <863326066@qq.com>
2019-01-23 15:21:35 +0800

    change input

--- ns-3/scratch/nrndn_new/main.cc
+++ ns-3/scratch/nrndn_new/main.cc
@@ -920,7 +920,7 @@ void nrndnExample::InstallTraffics()
 	SeedManager::SetSeed(random_seed);
 	// 2017.12.29 added by sy
 	// RSU不产生数据包 需要减去RSU的数量
-	UniformVariable rnd(0, nodes.GetN() - 190);
+	UniformVariable rnd(0, nodes.GetN() - 247);
 	std::cout << "(main.cc-InstallTraffics)插入事件：" << accidentNum << endl
 			  << endl;
 	if (random_accident)
