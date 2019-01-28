059a470077cebc9c2dd196dd006038c11521a74c
Sindyang <863326066@qq.com>
2019-01-28 16:47:17 +0800

    change addAccident

--- ns-3/scratch/nrndn_old/nrProducer.cc
+++ ns-3/scratch/nrndn_old/nrProducer.cc
@@ -228,8 +228,8 @@ void nrProducer::addAccident()
 	double end = m_stopTime.GetSeconds();
 	uint32_t t = 0;
 	double totalTime = 500 - start;
-	uint32_t totalCount = totalTime / 4;
-	UniformVariable nrnd(start, 200);
+	uint32_t totalCount = totalTime / 13;
+	UniformVariable nrnd(start, 250);
 	std::cout << "(nrProducer.cc-addAccident) 生产者 " << m_node->GetId() << "预计发送 " << totalCount << "个数据包" << std::endl;
 
 	uint32_t count = 0.8 * totalCount;
