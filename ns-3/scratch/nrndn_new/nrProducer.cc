d2f1a007860c6f5fb6afa891085c95e5bf392331
Sindyang <863326066@qq.com>
2019-02-22 19:16:35 +0800

    change interval

--- ns-3/scratch/nrndn_new/nrProducer.cc
+++ ns-3/scratch/nrndn_new/nrProducer.cc
@@ -286,7 +286,7 @@ void nrProducer::addAccident()
 	double end = m_stopTime.GetSeconds();
 	uint32_t t = 0;
 	double totalTime = 500 - start;
-	uint32_t totalCount = totalTime / 10;
+	uint32_t totalCount = totalTime / 7;
 	UniformVariable nrnd(start, 200);
 	std::cout << "(nrProducer.cc-addAccident) 生产者 " << m_node->GetId() << "预计发送 " << totalCount << "个数据包" << std::endl;
 
