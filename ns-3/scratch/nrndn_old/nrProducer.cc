b0d2998a8b195668946d1a1d08911223c1eae1f2
Sindyang <863326066@qq.com>
2019-03-03 19:59:50 +0800

    change freshness

--- ns-3/scratch/nrndn_old/nrProducer.cc
+++ ns-3/scratch/nrndn_old/nrProducer.cc
@@ -177,7 +177,7 @@ void nrProducer::OnSendingTrafficData()
 	dataName->append(m_postfix); //m_postfix is "/", seems OK
 	data->SetName(dataName);
 	// 2018.1.24
-	data->SetFreshness(Seconds(2.0));
+	data->SetFreshness(Seconds(5.0));
 	data->SetTimestamp(Simulator::Now());
 
 	data->SetSignature(m_rand.GetValue()); //just generate a random number
