544db17aac380fb365c602c02f0936c3f98c57b3
Sindyang <863326066@qq.com>
2019-03-14 17:09:45 +0800

    change end time

--- ns-3/scratch/nrndn_siukwan/nrProducer.cc
+++ ns-3/scratch/nrndn_siukwan/nrProducer.cc
@@ -368,7 +368,7 @@ void nrProducer::addAccident(double iType)
 
 	std::cout << "start " << start << std::endl;
 	std::cout << "end " << end << std::endl;
-	for (uint32_t dTime = 90; dTime < end-10; dTime += iType)
+	for (uint32_t dTime = 90; dTime < end-20; dTime += iType)
 	{
 		ScheduleAccident(dTime);
 		//std::cout<<"siu:"<<m_node->GetId()<<" add accident at "<< dTime <<endl;
