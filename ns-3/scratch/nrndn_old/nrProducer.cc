c043fc2af02ee3e72d50911aee91293f9be5d1de
Sindyang <863326066@qq.com>
2019-03-21 16:48:08 +0800

    change start and end time

--- ns-3/scratch/nrndn_old/nrProducer.cc
+++ ns-3/scratch/nrndn_old/nrProducer.cc
@@ -268,7 +268,7 @@ void nrProducer::addAccident(double iType)
 	double start = m_startTime.GetSeconds();
 	double end = m_stopTime.GetSeconds();
 
-	for (uint32_t dTime = 60; dTime < end-5; dTime += iType)
+	for (uint32_t dTime = start+20; dTime < end-10; dTime += iType)
 	{
 		ScheduleAccident(dTime);
 	}
