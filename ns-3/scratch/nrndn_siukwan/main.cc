6843d31933307f36d28da2d427a7cd8d43a6993b
Sindyang <863326066@qq.com>
2019-03-21 11:33:03 +0800

    change producer

--- ns-3/scratch/nrndn_siukwan/main.cc
+++ ns-3/scratch/nrndn_siukwan/main.cc
@@ -200,7 +200,7 @@ int main(int argc, char **argv)
 //-----------------------------------------------------------------------------
 //构造函数
 nrndnExample::nrndnExample() : random_seed(54321),
-							   certain_count(40),	//定点数量
+							   certain_count(50),	//定点数量
 							   certain_interval(14), //定点事件间隔
 							   random_accident(0),   //默认不随机
 							   size(3),
@@ -883,7 +883,7 @@ void nrndnExample::InstallTraffics()
 	SeedManager::SetSeed(random_seed);
 	UniformVariable rnd(0, nodes.GetN() - 190);
 	std::cout << "插入事件：" << accidentNum << endl;
-	if (0)//(random_accident)
+	if (random_accident)
 	{
 		for (uint32_t idx = 0; idx < certain_count; idx++)
 		{
