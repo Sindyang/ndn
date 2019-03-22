5b15929161ed2e81079e6665d663bced265d69ee
Sindyang <863326066@qq.com>
2019-03-22 22:35:29 +0800

    change interval

--- ns-3/scratch/nrndn_siukwan/main.cc
+++ ns-3/scratch/nrndn_siukwan/main.cc
@@ -201,7 +201,7 @@ int main(int argc, char **argv)
 //构造函数
 nrndnExample::nrndnExample() : random_seed(54321),
 							   certain_count(50),	//定点数量
-							   certain_interval(5), //定点事件间隔
+							   certain_interval(4), //定点事件间隔
 							   random_accident(0),   //默认不随机
 							   size(3),
 							   totalTime(36000),
