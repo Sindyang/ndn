0b23c149af97c7b43be1450b834254a84c8b7c9a
Sindyang <863326066@qq.com>
2019-03-28 09:27:54 +0800

    change producer

--- ns-3/scratch/nrndn_old/main.cc
+++ ns-3/scratch/nrndn_old/main.cc
@@ -200,7 +200,7 @@ int main(int argc, char **argv)
 //构造函数
 nrndnExample::nrndnExample() : random_seed(54321),
 							   certain_count(50),	//定点数量
-							   certain_interval(15), //定点事件间隔
+							   certain_interval(0), //定点事件间隔
 							   random_accident(0),   //默认不随机
 							   size(3),
 							   totalTime(36000),
@@ -893,7 +893,7 @@ void nrndnExample::InstallTraffics()
 	SeedManager::SetSeed(random_seed);
 	// 2017.12.29 added by sy
 	// RSU不产生数据包 需要减去RSU的数量
-	UniformVariable rnd(0, nodes.GetN() - 36);
+	UniformVariable rnd(0, nodes.GetN() - 190);
 	std::cout << "(main.cc-InstallTraffics)插入事件：" << accidentNum << endl
 			  << endl;
 	if (0)//(random_accident)
