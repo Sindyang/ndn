17159c913eea0070869a1e47014232f7715e541a
Sindyang <863326066@qq.com>
2019-04-05 12:00:53 +0800

    change distribution

--- ns-3/scratch/nrndn_new/nrProducer.cc
+++ ns-3/scratch/nrndn_new/nrProducer.cc
@@ -302,7 +302,7 @@ void nrProducer::addAccident()
 	UniformVariable nrnd(start+5, medium);
 	std::cout << "(nrProducer.cc-addAccident) 生产者 " << m_node->GetId() << "预计发送 " << totalCount << "个数据包" << std::endl;
 
-	uint32_t count = 0.7 * totalCount;
+	uint32_t count = 0.75 * totalCount;
 	while (count--)
 	{
 		t = nrnd.GetValue();
@@ -313,7 +313,7 @@ void nrProducer::addAccident()
 		}
 	}
 
-	count = 0.3 * totalCount;
+	count = 0.25 * totalCount;
 	UniformVariable nrndnew(medium, end-5);
 	while (count--)
 	{
